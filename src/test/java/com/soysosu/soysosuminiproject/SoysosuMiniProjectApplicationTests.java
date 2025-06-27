49b33ef77b9fa1b2963429203c7b1c59fe2321f0
yujin-0 <yujinjin0122@gmail.com>
2025-06-27 10:15:52 +0900

    first commit

---
+++ src/test/java/com/soysosu/soysosuminiproject/SoysosuMiniProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soysosu.soysosuminiproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoysosuMiniProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
